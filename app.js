7e4ed1379f40e7d66ae2847b85bc15210a769256
aaronguarnes <aaronguarnes@gmail.com>
2021-01-12 10:51:14 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
+const handlebars = require('handlebars');
+const router = express.Router();
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+
+const loginroute = require('./routes/login');
+
+app.use('/login', loginroute);
+
+//Hbs Config
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', function (req, res) {
+    res.render("main");
+});
+
+app.get('/login', function (req, res) {
+    res.render("login");
+});
+
+app.get('/register', function (req, res) {
+    res.render("register");
+});
+
