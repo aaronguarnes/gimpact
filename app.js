862f970e2f0cbb1a3209635e86c8faf2ee91fe78
aaronguarnes <aaronguarnes@gmail.com>
2021-01-14 19:08:21 +0800

    updated styles and mainpage

--- app.js
+++ app.js
@@ -36,7 +36,17 @@ app.get('/login', function (req, res) {
     res.render("login");
 });
 
+app.post('/login', function (req, res) {
+    //login function
+});
+
 app.get('/register', function (req, res) {
     res.render("register");
 });
+
+app.post('/register', function (req, res) {
+    //register function
+});
+
+
 
